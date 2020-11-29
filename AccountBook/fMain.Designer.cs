3a54d81f34df491b5cc26b450b062cdc018bb281
Yoojin oh <funluvin@naver.com>
2020-11-29 20:25:04 +0900

    0.2

--- AccountBook/fMain.Designer.cs
+++ AccountBook/fMain.Designer.cs
@@ -29,21 +29,236 @@ namespace AccountBook
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dv1 = new System.Windows.Forms.DataGridView();
+            this.dv2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dvCreditInfo = new System.Windows.Forms.DataGridView();
+            this.dvAccountInfo = new System.Windows.Forms.DataGridView();
+            this.dvSettingInfo = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dv2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dvCreditInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvAccountInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvSettingInfo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1094, 796);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridView3);
+            this.tabPage1.Controls.Add(this.dv2);
+            this.tabPage1.Controls.Add(this.dv1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1086, 770);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dv1
+            // 
+            this.dv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dv1.Location = new System.Drawing.Point(37, 346);
+            this.dv1.Name = "dv1";
+            this.dv1.RowTemplate.Height = 23;
+            this.dv1.Size = new System.Drawing.Size(240, 334);
+            this.dv1.TabIndex = 0;
+            // 
+            // dv2
+            // 
+            this.dv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dv2.Location = new System.Drawing.Point(428, 346);
+            this.dv2.Name = "dv2";
+            this.dv2.RowTemplate.Height = 23;
+            this.dv2.Size = new System.Drawing.Size(240, 334);
+            this.dv2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(789, 335);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 23;
+            this.dataGridView3.Size = new System.Drawing.Size(240, 334);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IMPORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "EXPORT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "TOTAL";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dvCreditInfo);
+            this.tabPage3.Controls.Add(this.dvAccountInfo);
+            this.tabPage3.Controls.Add(this.dvSettingInfo);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1086, 770);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Setting";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dvCreditInfo
+            // 
+            this.dvCreditInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvCreditInfo.Location = new System.Drawing.Point(805, 288);
+            this.dvCreditInfo.Name = "dvCreditInfo";
+            this.dvCreditInfo.RowTemplate.Height = 23;
+            this.dvCreditInfo.Size = new System.Drawing.Size(240, 334);
+            this.dvCreditInfo.TabIndex = 5;
+            // 
+            // dvAccountInfo
+            // 
+            this.dvAccountInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvAccountInfo.Location = new System.Drawing.Point(444, 299);
+            this.dvAccountInfo.Name = "dvAccountInfo";
+            this.dvAccountInfo.RowTemplate.Height = 23;
+            this.dvAccountInfo.Size = new System.Drawing.Size(240, 334);
+            this.dvAccountInfo.TabIndex = 4;
+            // 
+            // dvSettingInfo
+            // 
+            this.dvSettingInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvSettingInfo.Location = new System.Drawing.Point(53, 299);
+            this.dvSettingInfo.Name = "dvSettingInfo";
+            this.dvSettingInfo.RowTemplate.Height = 23;
+            this.dvSettingInfo.Size = new System.Drawing.Size(240, 334);
+            this.dvSettingInfo.TabIndex = 3;
             // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1094, 796);
+            this.Controls.Add(this.tabControl1);
             this.Name = "fMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.fMain_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dv2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dvCreditInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvAccountInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvSettingInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dv2;
+        private System.Windows.Forms.DataGridView dv1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dvCreditInfo;
+        private System.Windows.Forms.DataGridView dvAccountInfo;
+        private System.Windows.Forms.DataGridView dvSettingInfo;
     }
 }
 
--- AccountBook/fMain.cs
+++ AccountBook/fMain.cs
@@ -14,6 +14,7 @@ namespace AccountBook
     public partial class fMain : Form
     {
         DBMysql _db = new DBMysql();
+        int cnt = 0;
 
         public fMain()
         {
@@ -23,6 +24,56 @@ namespace AccountBook
         private void fMain_Load(object sender, EventArgs e)
         {
             _db.Connection();
+            InitSetting();
+        }
+
+        public void InitSetting()
+        {
+            Config.settingds = _db.SelectDetail(Config.Tables[(int)Config.eTName._setting], "import_info as '수입계정', export_info as '지출계정'");
+            dvSettingInfo.DataSource = Config.settingds.Tables[0];
+            cnt = 0;
+            Config.ImportInfo = new string[Config.settingds.Tables[0].Rows.Count];
+            Config.ExportInfo = new string[Config.settingds.Tables[0].Rows.Count];
+            foreach (DataRow r in Config.settingds.Tables[0].Rows)
+            {
+                if (r["수입계정"] != DBNull.Value)
+                {
+                    Config.ImportInfo[cnt] = r["수입계정"].ToString();
+                }
+                if (r["지출계정"] != DBNull.Value)
+                {
+                    Config.ExportInfo[cnt] = r["지출계정"].ToString();
+                }
+                cnt++;
+            }
+            Config.ImportInfo = Config.ImportInfo.Where(c => c != null).ToArray();
+            Config.ExportInfo = Config.ExportInfo.Where(c => c != null).ToArray();
+
+            cnt = 0;
+            string AccStr = "bank_name as '은행명', account_name as '계좌명', connect_card as '연결체크카드', first_balance as '최초잔액', memo as '메모'";
+            Config.accountds = _db.SelectDetail(Config.Tables[(int)Config.eTName._account], AccStr);
+            dvAccountInfo.DataSource = Config.accountds.Tables[0];
+            dvAccountInfo.Columns[3].DefaultCellStyle.Format = "###,##0";
+            dvAccountInfo.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            Config.AccountName = new string[Config.accountds.Tables[0].Rows.Count];
+            Config.ConnectCard = new string[Config.accountds.Tables[0].Rows.Count];
+            foreach (DataRow r in Config.accountds.Tables[0].Rows)
+            {
+                Config.AccountName[cnt] = r["계좌명"].ToString();
+                Config.ConnectCard[cnt] = r["연결체크카드"].ToString();
+                cnt++;
+            }
+
+            cnt = 0;
+            string CreditStr = "credit_bank as '신용카드사', card_name as '카드명', withdraw_account as '출금계좌', bank_name as '은행명', withdraw_date as '출금일', use_term as '사용실적기간'";
+            Config.creditds = _db.SelectDetail(Config.Tables[(int)Config.eTName._credit], CreditStr);
+            dvCreditInfo.DataSource = Config.creditds.Tables[0];
+            Config.CardName = new string[Config.creditds.Tables[0].Rows.Count];
+            foreach (DataRow r in Config.creditds.Tables[0].Rows)
+            {
+                Config.CardName[cnt] = r["카드명"].ToString();
+                cnt++;
+            }
         }
     }
 }
